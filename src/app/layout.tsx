aca1237686d213ef1de54c80fbbed6903c1873bb
ronitrai27 <108011380+ronitrai27@users.noreply.github.com>
2026-01-09 18:24:55 +0530

    auth setup with clerk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,16 @@ import type { Metadata } from "next";
 import { Inter, IBM_Plex_Mono } from "next/font/google";
 import { ThemeProvider } from "@/providers/theme-provider";
 import "./globals.css";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
+import { Button } from "@/components/ui/button";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -25,19 +35,35 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${inter.variable} ${plexMono.variable} antialiased`}
-      >
+    <ClerkProvider
+      appearance={{
+        theme: dark,
+      }}
+    >
+      <html lang="en" suppressHydrationWarning>
+        <body className={`${inter.variable} ${plexMono.variable} antialiased`}>
           <ThemeProvider
             attribute="class"
             defaultTheme="dark"
             enableSystem
             disableTransitionOnChange
           >
+            <header>
+              <SignedOut>
+                <SignInButton />
+              </SignedOut>
+              <SignUpButton>
+                <Button>Sign up</Button>
+              </SignUpButton>
+              <SignedIn>
+                <UserButton />
+              </SignedIn>
+            </header>
+
             {children}
           </ThemeProvider>
-      </body>
-    </html>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
