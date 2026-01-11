2998f813268b888bf8ad0a3633c9f90c2706a0dd
ronitrai27 <108011380+ronitrai27@users.noreply.github.com>
2026-01-12 01:46:39 +0530

    sentry tracking

--- src/inngest/client.ts
+++ src/inngest/client.ts
@@ -1,5 +1,7 @@
 import { Inngest } from "inngest";
+import { sentryMiddleware } from "@inngest/middleware-sentry";
 
 export const inngest = new Inngest({
-    id: "rox-ide"
+    id: "rox-ide",
+    middleware: [sentryMiddleware()]
 })
